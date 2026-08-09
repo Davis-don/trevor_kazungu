ebb51cc70e5bf17f07a6158c963a4b5fa57f9a21
Davis-don <davismugoikou@gmail.com>
2026-08-09 18:58:05 +0300

    added footer

--- client/src/components/footer/Footer.tsx
+++ client/src/components/footer/Footer.tsx
@@ -1,8 +1,15 @@
 import './footer.css'
+import Brandname from '../header/Brandname'
 
 function Footer() {
   return (
-    <div>Footer section</div>
+    <div className="overall-footer-container">
+      <Brandname />
+      <div className="botton-copyright-section">
+       <p>&copy; 2023 My App. All rights reserved.</p>
+      </div>
+      
+    </div>
   )
 }
 
--- client/src/layouts/Mainlayout.tsx
+++ client/src/layouts/Mainlayout.tsx
@@ -2,22 +2,29 @@ import Header from '../components/header/Header'
 import Footer from '../components/footer/Footer'
 import Sidebar from '../components/sidebar/Sidebar'
 import './mainlayout.css'
-import { useMountSidebar } from '../store/mountSidebar';
-function Mainlayout({  children,}: { children: React.ReactNode }) {
-  const { isMounted } = useMountSidebar();
+import { useMountSidebar } from '../store/mountSidebar'
+
+function Mainlayout({ children }: { children: React.ReactNode }) {
+  const { isMounted } = useMountSidebar()
+
   return (
-    <div className="overall-main-layout-container">
-        <Header />
-        <div className="main-layout-content-container">
-          <div className="actual-children-container">
-    {children}
+    <>
+      <Header />
+
+      <div className="main-layout-content-container">
+        <div className="actual-children-container">
+          {children}
+
+          <div className="sidebar-links-container">
+            {isMounted && <Sidebar />}
           </div>
-           <div className="sidebar-links-container">
-             {isMounted && <Sidebar />}
-           </div>
         </div>
-        <Footer/>
-    </div>
+      </div>
+
+      <div className="footer-main-layout">
+        <Footer />
+      </div>
+    </>
   )
 }
 
