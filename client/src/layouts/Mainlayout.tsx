caca5fd275233b7a5ad8a76571d169b859eb0ca9
Davis-don <davismugoikou@gmail.com>
2026-08-09 21:45:39 +0300

    Updated

--- client/src/layouts/Mainlayout.tsx
+++ client/src/layouts/Mainlayout.tsx
@@ -7,22 +7,24 @@ import { useMountSidebar } from '../store/mountSidebar'
 function Mainlayout({ children }: { children: React.ReactNode }) {
   const { isMounted } = useMountSidebar()
 
- return (
-  <>
-    <Header />
+  return (
+    <>
+      <Header />
 
-    <div className="main-layout-content-container">
-      <div className="actual-children-container">
-        {children}
-
+      <div className="main-layout-content-container">
+        <div className="actual-children-container">
+          {children}
+        </div>
+        
+        {/* Sidebar - Fixed position */}
         <div className="sidebar-links-container">
           {isMounted && <Sidebar />}
         </div>
       </div>
-    </div>
-<Footer />
-  </>
-)
+
+      <Footer />
+    </>
+  )
 }
 
 export default Mainlayout
